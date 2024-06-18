e34ec839d22a02eb20b2bd6f06c675cbe8b22caf
0ddbird <0ddbird@proton.me>
2024-06-18 13:58:35 +0200

    FEAT | Built CI/CD pipeline

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import sentry_sdk
+
 """
 django project settings
 """
@@ -125,3 +127,14 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+sentry_sdk.init(
+    dsn="https://da349f96068313367152a1c7ec663562@o4507025035558912.ingest.us.sentry.io/4507025037328384",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
