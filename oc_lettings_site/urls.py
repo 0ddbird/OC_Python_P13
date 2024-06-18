c1ee0fd96f10f29b09799fae5a36d596df13b92a
0ddbird <0ddbird@proton.me>
2024-06-18 13:56:42 +0200

    FEAT | Added Sentry monitoring

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -15,9 +15,14 @@ This module defines the URL patterns for the OC Lettings Site. It includes the f
 """
 
 
+def trigger_error(request):
+    return 1 / 0
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
+    path("sentry-debug/", trigger_error),
 ]
