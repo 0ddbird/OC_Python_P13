21a12ea4f7ad76d8704e94a4b33938cd07904916
Giacomo LLINARES <giacomo.llinares@gmail.com>
2024-12-27 10:07:35 +0100

    Changed HTML document title

--- update_container.sh
+++
@@ -1,4 +0,0 @@
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/mydjangoapp:${{ github.sha }}
-sudo docker stop mydjangoapp || true
-sudo docker rm mydjangoapp || true
-sudo docker run -d --name mydjangoapp -p 8000:8000 ${{ secrets.DOCKER_USERNAME }}/mydjangoapp:${{ github.sha }}
